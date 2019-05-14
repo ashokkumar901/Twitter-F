fe078f4fb1fdd996fbf216e838f7f8dbe7ffc54e
Ashok D <ashokkumar.d@way2online.co.in>
2019-05-14 12:21:58 +0530

    component changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { SigninComponent } from './components/signin/signin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { AuthGuard } from './guards/auth.guard';
 import { SignoutComponent } from './components/signout/signout.component';
-import { SuccesssigninComponent } from './components/successsignin/successsignin.component';
+import {SuccesssigninComponent} from './components/successsignin/successsignin.component';
 const routes: Routes = [
   { path: '', redirectTo: '/signin', pathMatch: 'full' },
   { path: 'signup', component: SignupComponent },
